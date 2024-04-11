87323cba85cef6c385d7c1ec2457ecff304433d0
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-04-11 21:12:38 +0300

    DOM-content

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let message = document.querySelector('#text')
+    message.textContent= "This is really cool!"
+    console.log(message)
+  });
